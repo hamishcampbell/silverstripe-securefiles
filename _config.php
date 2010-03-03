bea2d42e208552284fd062cb4ed962309a738c74
Hamish Campbell <hn.campbell@gmail.com>
2010-03-03 01:29:31 +0000

    Added comments and commented out the options. Must be enabled in your installation!

--- _config.php
+++ _config.php
@@ -1,6 +1,27 @@
 <?php
-DataObject::add_extension('File', 'SecureFileGroupPermissionDecorator');
-DataObject::add_extension('File', 'SecureFileDecorator');
+/**
+ * Secure File Module Configuratoin
+ *
+ * @package securefiles
+ * @author Hamish Campbell <hn.campbell@gmail.com>
+ * @copyright copyright (c) 2010, Hamish Campbell
+ */
 
+
+// CONTORLLER RULE, DO NOT MODIFY
 Director::addRules(50, array(ASSETS_DIR . '/$Action' => 'SecureFileController'));
+// -------------------------------
+
+
+/**
+ *  Apply optional permission methods here. Include them in the reverse
+ *  order that you would like them to appear in the CMS.
+ */
+// DataObject::add_extension('File', 'SecureFileMemberPermissionDecorator');
+// DataObject::add_extension('File', 'SecureFileGroupPermissionDecorator');
+
+// BASE DECORATOR, DO NOT MODIFY
+DataObject::add_extension('File', 'SecureFileDecorator');
+// -------------------------------
+
 ?>
