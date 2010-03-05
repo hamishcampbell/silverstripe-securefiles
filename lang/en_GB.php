fe9e7beb1dbc0952d37d1eb1f88949589f67727d
Hamish Campbell <hn.campbell@gmail.com>
2010-03-05 03:56:53 +0000

    Added en_GB lang file

---
+++ lang/en_GB.php
@@ -0,0 +1,25 @@
+<?php
+global $lang;
+
+$lang['en_GB']['SecureFiles']['SECUREFILEACCESS'] = 'Access to Secured Files';
+$lang['en_GB']['SecureFiles']['SECUREFILESETTINGS'] = 'Manage File Security Settings';
+$lang['en_GB']['SecureFiles']['SECUREFILETABNAME'] = 'Security';
+$lang['en_GB']['SecureFiles']['INHERITED'] = 'This folder is inheriting security settings from a parent folder.';
+$lang['en_GB']['SecureFiles']['SECUREFOLDER'] = 'Folder is secure.';
+$lang['en_GB']['SecureFiles']['FOLDERSECURITY'] = 'Folder Security';
+
+$lang['en_GB']['SecureFiles']['NONE'] = '(None)';
+$lang['en_GB']['SecureFiles']['NEVEREXPIRES'] =  'Never';
+
+// GROUP PERMISSION
+$lang['en_GB']['SecureFiles']['GROUPACCESSTITLE'] = 'Group Access';
+$lang['en_GB']['SecureFiles']['GROUPACCESSFIELD'] = 'Group Access Permissions';
+$lang['en_GB']['SecureFiles']['GROUPINHERITEDPERMS'] = 'Inherited Group Permissions';
+
+// MEMBER PERMISSOIN
+$lang['en_GB']['SecureFiles']['MEMBERACCESSTITLE'] = 'Member Access';
+$lang['en_GB']['SecureFiles']['MEMBERINHERITEDPERMS'] = 'Inherited Member Permissions';
+
+// TOKEN PERMISSION
+$lang['en_GB']['SecureFiles']['TOKENACCESSTITLE'] = 'Token Access';
+$lang['en_GB']['SecureFiles']['NOFILESINFOLDER'] = 'There are no files in this folder.';
