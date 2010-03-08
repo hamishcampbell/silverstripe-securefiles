f8a634dc472e4ffb11e8c0e895085b981a52ed29
Hamish Campbell <hn.campbell@gmail.com>
2010-03-08 08:15:48 +0000

    More comments

--- _config.php
+++ _config.php
@@ -15,10 +15,23 @@ AssetAdmin::require_css('securefiles/css/SecureFiles.css');
  *  Apply optional permission methods here. Include them in the reverse
  *  order that you would like them to appear in the CMS.
  */
+
+// Assign file security by individual member:
 // DataObject::add_extension('File', 'SecureFileMemberPermissionDecorator');
+
+// Assign file security by member group:
 // DataObject::add_extension('File', 'SecureFileGroupPermissionDecorator');
+
+// Create time-limited access tokens:
 // DataObject::add_extension('File', 'SecureFileTokenPermissionDecorator');
 
 // -------------------------------
 DataObject::add_extension('File', 'SecureFileDecorator');
+
+/**
+ * For large files or heavily trafficed sites use x-sendfile headers to by-pass
+ * file handling in PHP. Supported in lighttpd and in Apache with mod_xsendfile
+ * available at http://tn123.ath.cx/mod_xsendfile/
+ */
+// SecureFileController::UseXSendFile(true);
 
