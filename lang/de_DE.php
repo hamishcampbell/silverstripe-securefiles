2ba155e175a54e10cd7e447715114fa93a82059a
Hamish Campbell <hn.campbell@gmail.com>
2010-03-10 17:29:29 +0000

    Added german language pack, thanks Johannes

---
+++ lang/de_DE.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * German language pack
+ * Many thanks to Johannes Weberhofer
+ * @package securefiles
+ * @subpackage i18n
+ */
+global $lang;
+
+$lang['de_DE']['SecureFiles']['SECUREFILEACCESS'] = 'Zugriff zu gesicherten Dateien';
+$lang['de_DE']['SecureFiles']['SECUREFILESETTINGS'] = 'Ändern der Dateizugriffsrechte';
+$lang['de_DE']['SecureFiles']['SECUREFILETABNAME'] = 'Zugriffsrechte';
+$lang['de_DE']['SecureFiles']['INHERITED'] = 'Dieser Ordner übernimmt die Zugriffsrechte eines übergeordneten Ordners';
+$lang['de_DE']['SecureFiles']['SECUREFOLDER'] = 'Der Ordner ist gesichert.';
+$lang['de_DE']['SecureFiles']['FOLDERSECURITY'] = 'Ordner-Sicherheit';
+$lang['de_DE']['SecureFiles']['NONE'] = '(None)';
+$lang['en_US']['SecureFiles']['NEVEREXPIRES'] =  'Neimals';
+
+// GROUP PERMISSION
+$lang['de_DE']['SecureFiles']['GROUPACCESSTITLE'] = 'Gruppenberechtigungen';
+$lang['de_DE']['SecureFiles']['GROUPACCESSFIELD'] = 'Gruppenberechtigungen für diesen Ordner';
+$lang['de_DE']['SecureFiles']['GROUPINHERITEDPERMS'] = 'Übernommene Gruppenberechtigungen';
+
+// MEMBER PERMISSOIN
+$lang['de_DE']['SecureFiles']['MEMBERACCESSTITLE'] = 'Benutzerberechtigungen';
+$lang['de_DE']['SecureFiles']['MEMBERINHERITEDPERMS'] = 'Übernommene Benutzerberechtigungen';
