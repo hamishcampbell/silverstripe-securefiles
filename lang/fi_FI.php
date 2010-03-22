ed42405141cbfa5c6d0271b053a9e462f7aba2a2
Hamish Campbell <hn.campbell@gmail.com>
2010-03-22 20:41:33 +0000

    Finnish and Swedish language packs, thanks Steeeo!

---
+++ lang/fi_FI.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Finnish language pack
+ * Many thanks to Steeeo (http://silverstripe.org/ForumMemberProfile/show/8115)
+ * @package securefiles
+ * @subpackage i18n
+ */
+global $lang;
+
+$lang['fi_FI']['SecureFiles']['SECUREFILEACCESS'] = 'Pääsy suojattuihin tiedostoihin';
+$lang['fi_FI']['SecureFiles']['SECUREFILESETTINGS'] = 'Hallitse tiedostojen turvallisuusasetuksia';
+$lang['fi_FI']['SecureFiles']['SECUREFILETABNAME'] = 'Turvallisuus';
+$lang['fi_FI']['SecureFiles']['INHERITED'] = 'Tämä kansio perii yläkansion turvallisuusasetukset.';
+$lang['fi_FI']['SecureFiles']['SECUREFOLDER'] = 'Kansio on turvallinen.';
+$lang['fi_FI']['SecureFiles']['FOLDERSECURITY'] = 'Kansion turvallisuus';
+$lang['fi_FI']['SecureFiles']['NONE'] = '(Tyhjä)';
+$lang['en_US']['SecureFiles']['NEVEREXPIRES'] =  'Ei vanhene';
+
+// GROUP PERMISSION
+$lang['fi_FI']['SecureFiles']['GROUPACCESSTITLE'] = 'Ryhmän pääsyoikeudet';
+$lang['fi_FI']['SecureFiles']['GROUPACCESSFIELD'] = 'Ryhmän käyttöoikeudet';
+$lang['fi_FI']['SecureFiles']['GROUPINHERITEDPERMS'] = 'Perityt ryhmäoikeudet';
+
+// MEMBER PERMISSOIN
+$lang['fi_FI']['SecureFiles']['MEMBERACCESSTITLE'] = 'Käyttäjän pääsyoikeudet';
+$lang['fi_FI']['SecureFiles']['EMBERINHERITEDPERMS'] = 'Perityt käyttäjäoikeudet';
---
+++ lang/sv_SE.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Swedish language pack
+ * Many thanks to Steeeo (http://silverstripe.org/ForumMemberProfile/show/8115)
+ * @package securefiles
+ * @subpackage i18n
+ */
+global $lang;
+
+$lang['sv_SE']['SecureFiles']['SECUREFILEACCESS'] = 'Åtkomst till skyddade filer';
+$lang['sv_SE']['SecureFiles']['SECUREFILESETTINGS'] = 'Redigera säkerhetsinställningar för filer';
+$lang['sv_SE']['SecureFiles']['SECUREFILETABNAME'] = 'Säkerhet';
+$lang['sv_SE']['SecureFiles']['INHERITED'] = 'Denna mapp ärver säkerhetsinställningar från en överliggande mapp.';
+$lang['sv_SE']['SecureFiles']['SECUREFOLDER'] = 'Mappen är säker.';
+$lang['sv_SE']['SecureFiles']['FOLDERSECURITY'] = 'Mappsäkerhet';
+$lang['sv_SE']['SecureFiles']['NONE'] = '(Inget)';
+$lang['en_US']['SecureFiles']['NEVEREXPIRES'] =  'Aldrig';
+
+// GROUP PERMISSION
+$lang['sv_SE']['SecureFiles']['GROUPACCESSTITLE'] = 'Gruppåtkomst';
+$lang['sv_SE']['SecureFiles']['GROUPACCESSFIELD'] = 'Grupprättigheter';
+$lang['sv_SE']['SecureFiles']['GROUPINHERITEDPERMS'] = 'Ärvda grupprättigheter';
+
+// MEMBER PERMISSOIN
+$lang['sv_SE']['SecureFiles']['MEMBERACCESSTITLE'] = 'Användaråtkomst';
+$lang['sv_SE']['SecureFiles']['EMBERINHERITEDPERMS'] = 'Ärvda användarrättigheter';
