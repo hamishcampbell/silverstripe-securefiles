4584a2f69bca991a93f1a29183674aa9168b8d87
Hamish Campbell <hn.campbell@gmail.com>
2010-05-02 21:17:58 +0000

    Added Dutch language pack

---
+++ lang/nl_NL.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Dutch (The Netherlands) language pack
+ * Many thanks to Martijn
+ * @package securefiles
+ * @subpackage i18n
+ */
+global $lang;
+
+$lang['nl_NL']['SecureFiles']['SECUREFILEACCESS'] = 'Toegang tot beveiligde bestanden';
+$lang['nl_NL']['SecureFiles']['SECUREFILESETTINGS'] = 'Beheer bestandsbeveiligingsinstellingen';
+$lang['nl_NL']['SecureFiles']['SECUREFILETABNAME'] = 'Beveiliging';
+$lang['nl_NL']['SecureFiles']['INHERITED'] = 'Deze map neemt de beveiligingsinstellingen van de bovenliggende map over.';
+$lang['nl_NL']['SecureFiles']['SECUREFOLDER'] = 'Map is beveiligd.';
+$lang['nl_NL']['SecureFiles']['FOLDERSECURITY'] = 'Map beveiliging';
+$lang['nl_NL']['SecureFiles']['NONE'] = '(Geen)';
+$lang['nl_NL']['SecureFiles']['NEVEREXPIRES'] =  'Nooit';
+
+// GROUP PERMISSION
+$lang['nl_NL']['SecureFiles']['GROUPACCESSTITLE'] = 'Groepstoegang';
+$lang['nl_NL']['SecureFiles']['GROUPACCESSFIELD'] = 'Groepsmachtigingen';
+$lang['nl_NL']['SecureFiles']['GROUPINHERITEDPERMS'] = 'Overgenomen groepsmachtigingen';
+
+// MEMBER PERMISSION
+$lang['nl_NL']['SecureFiles']['MEMBERACCESSTITLE'] = 'Gebruikerstoegang';
+$lang['nl_NL']['SecureFiles']['MEMBERINHERITEDPERMS'] = 'Overgenomen gebruikersmachtigingen';
+
+// TOKEN PERMISSION
+$lang['nl_NL']['SecureFiles']['TOKENACCESSTITLE'] = 'Authorisatiecode toegang';
+$lang['nl_NL']['SecureFiles']['NOFILESINFOLDER'] = 'Deze map bevat geen bestanden.';
